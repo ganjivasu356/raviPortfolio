b3d643d13a13bb72cb0c4eced0c786c11c8c1b03
Vasu Ganji <ganjivasu356@gmail.com>
2024-12-26 23:23:28 +0530

    updated the package.json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.tsx
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./AppLayout";
@@ -9,34 +8,36 @@ import Experience from "./components/Experience";
 import Awards from "./components/Awards";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <AppLayout />,
+        errorElement: <Error />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+          },
+          {
+            path: "/articles",
+            element: <Articles />,
+          },
+          {
+            path: "/experience",
+            element: <Experience />,
+          },
+          {
+            path: "/awards",
+            element: <Awards />,
+          },
+        ],
+      },
+    ],
     {
-      path: "/",
-      element: <AppLayout />,
-      errorElement: <Error />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/articles",
-          element: <Articles />,
-        },
-        {
-          path: "/experience",
-          element: <Experience />,
-        },
-        {
-          path: "/awards",
-          element: <Awards />,
-        },
-      ],
-    },
-    {
-      basename: "/raviPortfolio",
-    },
-  ]);
+      basename: "/raviPortfolio", // Match your GitHub Pages subpath
+    }
+  );
 
   return <RouterProvider router={router} />;
 }
